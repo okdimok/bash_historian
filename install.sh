16eb579e8aacb1802fdfd0aa532e91a524d07b95
Dima Mironov <okdimok@gmail.com>
2020-01-26 02:07:29 +0300

    trying to add history locations

--- install.sh
+++ install.sh
@@ -4,10 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 function __bak_file () {
     [ -f $1 ] && cp $1 "${1}.bash_historian.bak" 
 }
-# Currently, only default history location is backed up. No way to learn, where the history is stored in an interactive session. Even starting an interactive session inside and sourcing bashrc doesn't help 
-__bak_file ${HOME}/.bash_history
 __bak_file ${HOME}/.bashrc
 __bak_file ${HOME}/.bash_aliases
+__bak_file ${HOME}/.bash_history
+# Currently, only default history location is backed up. No way to learn, where the history is stored in an interactive session. Even starting an interactive session inside and sourcing bashrc doesn't help
+# this outputs a HISTFILE to a file
+# doesn't work yet
+#bash --init-file <(echo ". ${HOME}/.bashrc; set | grep HISTFILE | grep -v SIZE | cut -d = -f 2  > /tmp/histfile")
+#HF=`cat /tmp/histfile`
+
 
 sed -i '/\(HIST\|PROMPT_COMMAND\|hist\|ignoreboth\|ignoredups\|ignorespace\)/ s/^/#/' ${HOME}/.bashrc
 sed -i '/\(HIST\|PROMPT_COMMAND\|hist\|ignoreboth\|ignoredups\|ignorespace\)/ s/$/ ### commented out by Bash Historian/' ${HOME}/.bashrc
