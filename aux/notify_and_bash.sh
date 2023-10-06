368bc200d59e3fdb37073b1fda561ab5a33dbe2f
Dmitry Mironov <dmitrym@nvidia.com>
2023-10-06 06:49:48 -0700

    proper code formatting

--- aux/notify_and_bash.sh
+++ aux/notify_and_bash.sh
@@ -2,5 +2,5 @@
 cmd=$1
 [[ -z $cmd ]] && cmd=/bin/bash
 echo -e "\07" # bell
-nsslack "" "▶ Your new interactive job <code>$cmd</code> has just started on <code>$(hostname)</code>"
+nsslack "" "▶ Your new interactive job \`$cmd\` has just started on \`$(hostname)\`"
 $cmd
